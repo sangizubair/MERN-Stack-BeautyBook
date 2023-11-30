a929b1220e0246f973489b5a5def295143496e74
Osama369 <umuhammadusamaqureshi@gmail.com>
2023-11-30 10:47:19 -0800

    frontend setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
-
 import './App.css'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Homepage from './pages/Homepage.jsx'
-import Register from './pages/Register.jsx'
-import Login from './pages/Login.jsx'
-
 function App() {
-  
 
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Homepage/>}/>
-          <Route path='register' element={<Register/>}/>
-          <Route path='login' element={<Login/>}/>
-        </Routes>
-      </BrowserRouter>
+      <h1 className="text-3xl font-bold underline text-red-600 align-center">
+       BeautyBook
+      </h1>
     </>
   )
 }
